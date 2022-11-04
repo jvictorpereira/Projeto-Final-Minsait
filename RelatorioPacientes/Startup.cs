1521aebaae9e8e21d1844bc83a7dbb4f801021e1
indra <indra@21LAPBRJ132NC20>
2022-11-03 23:34:03 -0300

    Adicionar arquivos de projeto.

---
+++ RelatorioPacientes/Startup.cs
@@ -0,0 +1,36 @@
+﻿namespace RelatorioPacientes
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+        }
+
+        public void Configure(WebApplication app, IWebHostEnvironment environment)
+        {
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+        }
+    }
+}
